8de1ad314acd4184fbdaccb92da324e446b962b5
NawafSwe <nawaf.jr9@hotmail.com>
2020-10-30 23:32:34 +0300

    creating user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,17 @@
+//user can have
 //avatar url
 //name
 //roles []
+/* ------------ Requiring Packages ------------ */
+const mongoose = require('mongoose');
+/* ------------ Creating Schemas edited By ------------ */
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    avatarUrl: {type: String, required: true}
+});
+/* ------------ Creating Mongoose Model ------------ */
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
+
+
